21f1682905b3c4f8b259cfc1571e618c5715d422
Bladefurey <bladefurey@gmail.com>
2020-10-18 02:16:30 +0630

    hr

---
+++ php/department.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<html lang="en">
+<head>
+<title>deaprtment</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" name="dpt_name" id="dpt_name" placeholder="Department Name">
+        <br><br>
+        <input type="email" name="email" id="email" placeholder="Email">
+        <br><br>
+        <input type="text" name="phone" id="phone" placeholder="Phone Number">
+        <br><br>
+        <button type="submit" id="subm">Submit</button>
+    </form>
+</body>
+</html>
